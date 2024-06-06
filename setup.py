78e9814b4f107a7d17b7e80e1d717b9ed3c093ce
Artur Porto <110861215+Artur-Porto@users.noreply.github.com>
2024-06-06 18:30:54 -0300

    Update setup.py
    Start writing setup.py code

--- setup.py
+++ setup.py
@@ -1,1 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='graph_clustering_framework',
+    version='0.1',
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+        'numpy',
+        'scipy',
+        'matplotlib',
+        'networkx',
+        'scikit-learn'
+    ],
+    
+    entry_points={
+        'console_scripts': [
+            'example_script=src.example:main',
+        ],
+    },
+    author='Seu Nome',
+    author_email='aartur.porto@gmail.com',
+    description='A framework for graph construction and data clustering',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Artur-Porto/graph-clustering-framework',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
 
