e1ea62e5dd7077506499928c11e7650626013546
Artur Porto <110861215+Artur-Porto@users.noreply.github.com>
2024-06-11 18:11:50 -0300

    Create plot_grafico2.py

---
+++ src/graph_visualization/plot_grafico2.py
@@ -0,0 +1,12 @@
+#Function used to vizualize the data base (Poinst (X,y) in space -- pre graph clustering)
+
+def plot_grafico2(X, y, colors, G=None):
+    fig = plt.figure(figsize=(8, 6))
+    ax = fig.add_subplot(111)
+    ax.scatter(X[:, 0], X[:, 1], c=y, cmap=ListedColormap(colors), edgecolor='k')
+    if G is not None:
+        for edge in G.es():
+            i, j = edge.tuple[0], edge.tuple[1]
+            line = numpy.array([X[i], X[j]])
+            ax.plot(line[:, 0], line[:, 1], color='black')
+    plt.show()
