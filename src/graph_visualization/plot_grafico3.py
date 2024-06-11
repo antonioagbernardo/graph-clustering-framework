b99d504b3e33744f8a08eed371244066b89028a3
Artur Porto <110861215+Artur-Porto@users.noreply.github.com>
2024-06-11 18:12:32 -0300

    Create plot_grafico3.py

---
+++ src/graph_visualization/plot_grafico3.py
@@ -0,0 +1,10 @@
+def plot_grafico3(X, y, colors, G=None):
+    fig = plt.figure(figsize=(8, 6))
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(X[:, 0], X[:, 1],  X[:, 2], c=y, cmap=ListedColormap(colors), edgecolor='k')
+    if G is not None:
+        for edge in G.es():
+            i, j = edge.tuple[0], edge.tuple[1]
+            line = np.array([X[i], X[j]])
+            ax.plot(line[:, 0], line[:, 1],  line[:, 2], color='black')
+    plt.show()
