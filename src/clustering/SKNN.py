6985fd3066f0e2063dd8f8b35500e7f1eee16485
Artur Porto <110861215+Artur-Porto@users.noreply.github.com>
2024-06-11 18:55:51 -0300

    Create SKNN.py

---
+++ src/clustering/SKNN.py
@@ -0,0 +1,56 @@
+def sknn(X, kmax, mode='distance', metric='euclidean'):
+    """Summary or Description of the Function
+
+    Parameters:
+    argument1 (int): Description of arg1
+
+    Returns:
+    int:Returning value
+
+   """
+    # Compute distance matrix
+    D = euclidean_distances(X, X)
+
+    # Compute closeness centrality
+    closeness = []
+    for i in range(0, D.shape[0]):
+        closeness.append(1 / sum(D[i,:]))
+    closeness = list(zip(list(range(0, D.shape[0])), closeness))
+    closeness.sort(key=lambda x:x[1], reverse=False)
+    g1 = Graph(D.shape[0])
+    kdtree = spatial.KDTree(X)
+    weights = []
+
+    # Sequential k-NN based on closseness centrality
+    k1 = 3
+    while True:
+        for i, value in closeness:
+            obj_knn = kdtree.query(X[i,:], k=(kmax + 1))
+            for key, j in enumerate(obj_knn[1]):
+                if i == j or g1.degree(j) >= k1 or D[i, j] <= 0.0:
+                    continue
+                if g1.are_connected(i, j) == False:
+                    g1.add_edge(i, j)
+                    weights.append(D[i, j])
+                break
+
+        k1 += 1
+        degree = np.array(g1.degree())
+        indices = np.where(degree < kmax)[0]
+        if len(indices) == 0 or k > kmax:
+            break
+
+    # Constrained k-NN by kmax
+    for i in indices:
+        obj_knn = kdtree.query(X[i,:], k=(kmax + 1))
+        for key, j in enumerate(obj_knn[1]):
+            if i == j or g1.degree(j) >= kmax or D[i, j] <= 0.0:
+                continue
+            if g1.are_connected(i, j) == False:
+                g1.add_edge(i, j)
+                weights.append(D[i, j])
+            if g1.degree(i) >= kmax:
+                break
+
+    g1.es['weight'] = weights
+    return g1
