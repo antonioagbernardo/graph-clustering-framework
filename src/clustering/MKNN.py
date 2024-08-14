7f8dd6d7867fc35c2e2cdc880f8e48211c5694ad
Artur Porto <110861215+Artur-Porto@users.noreply.github.com>
2024-08-14 17:42:09 -0300

    Update MKNN.py

--- src/clustering/MKNN.py
+++ src/clustering/MKNN.py
@@ -1,11 +1,22 @@
+from MST import mst_graph
+
 def components(W):
   g = Graph.Weighted_Adjacency(W.todense(), mode='undirected', attr='weight', loops=False)
   return len(g.connected_components())
 
-def MKNN(X,k):
-  W = kneighbors_graph(X, k, mode='distance', metric='euclidean', include_self=False)
-  W = W.minimum(W.T)
-  if components(W) > 1:
-     W = W + mst_graph(X,'euclidean')
-  g2 = Graph.Weighted_Adjacency(W.todense(), mode='undirected', attr='weight', loops=False)
-  return g2
+
+def MKNN(X,k,metric):
+  if metric == 'euclidean':
+    W = kneighbors_graph(X, k, mode='distance', metric='euclidean', include_self=False)
+    W = W.minimum(W.T)
+    if components(W) > 1:
+      W = W + mst_graph(X,'euclidean')
+    g2 = Graph.Weighted_Adjacency(W.todense(), mode='undirected', attr='weight', loops=False)
+    return g2, W
+  else:
+    W = kneighbors_graph(X, k, mode='distance', metric='cosine', include_self=False)
+    W = W.minimum(W.T)
+    if components(W) > 1:
+      W = W + mst_graph(X,'cosine')
+    g2 = Graph.Weighted_Adjacency(W.todense(), mode='undirected', attr='weight', loops=False)
+    return g2, W
