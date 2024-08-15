e4e51628e554c78317a8e97a0a2a3fc1cb9e785d
Artur Porto <110861215+Artur-Porto@users.noreply.github.com>
2024-08-15 18:57:28 -0300

    Update Epsilon.py
    Returns now W,g

--- src/clustering/Epsilon.py
+++ src/clustering/Epsilon.py
@@ -1,5 +1,9 @@
+#EPSILON
+
+#Código
+
 def Epsilon(X,e):
   W = radius_neighbors_graph(X, e, mode='distance', metric='euclidean', include_self=False)
   W = 0.5 * (W + W.T) # make connectivity symmetric
   g = Graph.Weighted_Adjacency(W.todense(), mode='undirected', attr='weight', loops=False)
-  return g
+  return g, W
