67ad7c4a5012b7847c53ff33cf08b2165de5ced1
Artur Porto <110861215+Artur-Porto@users.noreply.github.com>
2024-08-15 18:56:47 -0300

    Update KNN.py
    Returns now W,g

--- src/clustering/KNN.py
+++ src/clustering/KNN.py
@@ -1,17 +1,20 @@
+#KNN
+
+#Código
 def KNN(X,k,metric):
 
   if metric == 'euclidean':
-    W = kneighbors_graph(X, 4, mode='distance', metric='euclidean', include_self=False)
+    W = kneighbors_graph(X, k, mode='distance', metric='euclidean', include_self=False)
     W = 0.5 * (W + W.T)
     g = Graph.Weighted_Adjacency(W.todense(), mode='undirected', attr='weight', loops=False)
 
-    return g
+    return g, W
 
   if metric == 'cosine':
 
-    W = kneighbors_graph(X, 4, mode='distance', metric='cosine', include_self=False)
+    W = kneighbors_graph(X, k, mode='distance', metric='cosine', include_self=False)
     W = 0.5 * (W + W.T)
     g = Graph.Weighted_Adjacency(W.todense(), mode='undirected', attr='weight', loops=False)
 
-    return g
+    return g, W
   
