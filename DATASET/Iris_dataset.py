c1e8aa94c7c34179885670c8b3409859f92e6f37
Artur Porto <110861215+Artur-Porto@users.noreply.github.com>
2024-08-17 10:22:42 -0300

    Create Iris_dataset.py

---
+++ DATASET/Iris_dataset.py
@@ -0,0 +1,236 @@
+# Iris Dataset
+
+## Upload and Vizualizations
+
+Iris_dataset = pd.read_csv('Iris_dataset.csv')
+
+Iris_dataset.columns
+
+Iris_dataset.info()
+
+Iris_dataset.head()
+
+sns.set_style("whitegrid")
+sns.pairplot(Iris_dataset,hue="species",size=3);
+plt.show()
+
+px.scatter_3d(Iris_dataset, "sepal_length", "sepal_width", "petal_length",
+             color="species", color_discrete_map = {"Joly": "blue", "Bergeron": "violet", "Coderre":"pink"})
+
+## Graph Constructing
+
+### MST
+
+#### Sepal Length - Sepal Width -  Petal Length -   Petal Width
+
+X = np.array(Iris_dataset[['sepal_length', 'sepal_width','petal_length', 'petal_width']])
+
+y = Iris_dataset['species']
+le = LabelEncoder()
+y = le.fit_transform(y)
+
+g, W = MST(X,'euclidean')
+
+print('Score Wrong Edges:', score_we(W, y))
+
+
+# Plot
+
+# Grafo 2D
+plot_2d_graph(g, y=y, X=X, ind=range(0, g.vcount()), colors=colors)
+
+plot_grafico2(X,y,colors,g)
+
+#Grafo 3D
+plot_grafico3(X, y, colors, g)
+
+
+X = np.array(Iris_dataset[['sepal_length', 'sepal_width','petal_length', 'petal_width']])
+
+y = Iris_dataset['species']
+le = LabelEncoder()
+y = le.fit_transform(y)
+
+g, W = MST(X,'cosine')
+
+print('Score Wrong Edges:', score_we(W, y))
+
+
+# Plot
+
+# Grafo 2D
+plot_2d_graph(g, y=y, X=X, ind=range(0, g.vcount()), colors=colors)
+
+plot_grafico2(X,y,colors,g)
+
+#Grafo 3D
+plot_grafico3(X, y, colors, g)
+
+### KNN
+
+#### Sepal Length - Sepal Width -  Petal Length ( Euclidean)
+
+X = np.array(Iris_dataset[['sepal_length', 'sepal_width', 'petal_width']])
+
+y = Iris_dataset['species']
+le = LabelEncoder()
+y = le.fit_transform(y)
+
+g, W = KNN(X,3,'euclidean')
+
+print('Score Wrong Edges:', score_we(W, y))
+
+
+# Plot
+
+# Grafo 2D
+plot_2d_graph(g, y=y, X=X, ind=range(0, g.vcount()), colors=colors)
+
+plot_grafico2(X,y,colors,g)
+
+#Grafo 3D
+plot_grafico3(X, y, colors, g)
+
+##### Clustering example
+
+#Aplicando Kmeans
+kmeans = KMeans(n_clusters=3, random_state=42, n_init=10)
+kmeans.fit(X)
+
+labels_kmeans = kmeans.labels_
+
+#Aplicando Fastgreedy
+g, W = KNN(X, k=5, metric='euclidean')
+
+# Convert W to a dense matrix
+W_dense = W.toarray()
+
+# Get the edge list from the graph
+edges = g.get_edgelist()
+
+# Create a weights vector with the same length as the number of edges
+edge_weights = np.zeros(len(edges))
+
+# Assign weights to each edge using the dense matrix
+for i, edge in enumerate(edges):
+    edge_weights[i] = W_dense[edge[0], edge[1]]
+
+# Now you can pass edge_weights to the community_fastgreedy method
+fastgreedy = Graph.community_fastgreedy(g, weights=edge_weights)
+labels_fastgreedy = fastgreedy.as_clustering().membership
+
+
+#Comparing
+plt.scatter(X[:, 0], X[:, 1], c=labels_kmeans)
+plt.title('Kmeans')
+plt.show()
+
+plt.scatter(X[:, 0], X[:, 1], c=labels_fastgreedy)
+plt.title('Fastgreedy + KNN')
+plt.show()
+
+
+#NMI (Normalized Mutual Information)
+labels_kmeans = kmeans.labels_
+labels_fastgreedy = fastgreedy.as_clustering().membership
+
+nmi_kmeans = normalized_mutual_info_score(y, labels_kmeans)
+nmi_fastgreedy = normalized_mutual_info_score(y, labels_fastgreedy)
+
+print()
+print("NMI K-Means:", nmi_kmeans)
+print("NMI Fastgreedy + KNN:", nmi_fastgreedy)
+print()
+
+plt.bar(["K-Means", "Fastgreedy + KNN"], [nmi_kmeans, nmi_fastgreedy], color=["blue", "green"])
+plt.xlabel("Algoritmo")
+plt.ylabel("NMI")
+plt.title("Pontuação de NMI")
+plt.show()
+
+#### Sepal Length - Sepal Width -  Petal Length (Cosine)
+
+X = np.array(Iris_dataset[['sepal_length', 'sepal_width', 'petal_width']])
+
+y = Iris_dataset['species']
+le = LabelEncoder()
+y = le.fit_transform(y)
+
+g, W = KNN(X,3,'cosine')
+
+print('Score Wrong Edges:', score_we(W, y))
+
+
+# Plot
+
+# Grafo 2D
+plot_2d_graph(g, y=y, X=X, ind=range(0, g.vcount()), colors=colors)
+
+plot_grafico2(X,y,colors,g)
+
+#Grafo 3D
+plot_grafico3(X, y, colors, g)
+
+#### Sepal Length - Sepal Width
+
+# @title
+X = np.array(Iris_dataset[['sepal_length', 'sepal_width']])
+
+y = Iris_dataset['species']
+le = LabelEncoder()
+y = le.fit_transform(y)
+
+g, W = KNN(X,3,'euclidean')
+
+print('Score Wrong Edges:', score_we(W, y))
+
+
+# Plot
+
+# Grafo 2D
+plot_2d_graph(g, y=y, X=X, ind=range(0, g.vcount()), colors=colors)
+
+plot_grafico2(X,y,colors,g)
+
+#### Petal Length -   Petal Width
+
+# @title
+X = np.array(Iris_dataset[['petal_length', 'petal_width']])
+
+y = Iris_dataset['species']
+le = LabelEncoder()
+y = le.fit_transform(y)
+
+g, W = KNN(X,3,'euclidean')
+
+print('Score Wrong Edges:', score_we(W, y))
+
+# Plot
+
+# Grafo 2D
+plot_2d_graph(g, y=y, X=X, ind=range(0, g.vcount()), colors=colors)
+
+plot_grafico2(X,y,colors,g)
+
+### KNN - Cosine
+
+X = np.array(Iris_dataset[['sepal_length', 'sepal_width','petal_length', 'petal_width']])
+
+y = Iris_dataset['species']
+le = LabelEncoder()
+y = le.fit_transform(y)
+
+g, W = KNN(X,3,'cosine')
+
+print('Score Wrong Edges:', score_we(W, y))
+
+
+# Plot
+
+# Grafo 2D
+plot_2d_graph(g, y=y, X=X, ind=range(0, g.vcount()), colors=colors)
+
+plot_grafico2(X,y,colors,g)
+
+#Grafo 3D
+plot_grafico3(X, y, colors, g)
