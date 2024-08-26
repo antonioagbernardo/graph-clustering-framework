aca41da8db84866956d5b0b1a1f5ad6bc1493eb4
Artur Porto <110861215+Artur-Porto@users.noreply.github.com>
2024-08-26 20:51:38 -0300

    Update KNN.py

--- src/clustering/KNN.py
+++ src/clustering/KNN.py
@@ -1,6 +1,7 @@
 #KNN
 
 #Código
+'''
 from sklearn.neighbors import kneighbors_graph
 from igraph import Graph
 
@@ -20,4 +21,28 @@ def KNN(X,k,metric):
     g = Graph.Weighted_Adjacency(W.todense(), mode='undirected', attr='weight', loops=False)
 
     return g, W
+ ''' 
+# KNN
+
+# Importações
+from sklearn.neighbors import kneighbors_graph as knn_graph
+from igraph import Graph
+
+def KNN(X, k, metric):
   
+    if metric == 'euclidean':
+        W = knn_graph(X, k, mode='distance', metric='euclidean', include_self=False)
+        W = 0.5 * (W + W.T)
+        g = Graph.Weighted_Adjacency(W.todense(), mode='undirected', attr='weight', loops=False)
+
+        return g, W
+
+    elif metric == 'cosine':
+        W = knn_graph(X, k, mode='distance', metric='cosine', include_self=False)
+        W = 0.5 * (W + W.T)
+        g = Graph.Weighted_Adjacency(W.todense(), mode='undirected', attr='weight', loops=False)
+
+        return g, W
+
+    else:
+        raise ValueError("Métrica não suportada. Use 'euclidean' ou 'cosine'.")
