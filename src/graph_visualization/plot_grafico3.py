405f3be1e22ea208dadb7442037707c80b6815b4
Artur Porto <110861215+Artur-Porto@users.noreply.github.com>
2024-08-26 21:40:52 -0300

    Update plot_grafico3.py

--- src/graph_visualization/plot_grafico3.py
+++ src/graph_visualization/plot_grafico3.py
@@ -1,3 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.colors import ListedColormap
+import igraph
+from mpl_toolkits.mplot3d import Axes3D
+
 def plot_grafico3(X, y, colors, G=None):
     fig = plt.figure(figsize=(8, 6))
     ax = fig.add_subplot(111, projection='3d')
