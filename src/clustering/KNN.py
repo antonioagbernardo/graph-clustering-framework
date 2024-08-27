852c0c7d8e9a61fdd12647931cbed702482f6c35
Artur Porto <110861215+Artur-Porto@users.noreply.github.com>
2024-08-26 21:27:48 -0300

    Update KNN.py

--- src/clustering/KNN.py
+++ src/clustering/KNN.py
@@ -1,27 +1,3 @@
-#KNN
-
-#Código
-'''
-from sklearn.neighbors import kneighbors_graph
-from igraph import Graph
-
-def KNN(X,k,metric):
-
-  if metric == 'euclidean':
-    W = kneighbors_graph(X, k, mode='distance', metric='euclidean', include_self=False)
-    W = 0.5 * (W + W.T)
-    g = Graph.Weighted_Adjacency(W.todense(), mode='undirected', attr='weight', loops=False)
-
-    return g, W
-
-  if metric == 'cosine':
-
-    W = kneighbors_graph(X, k, mode='distance', metric='cosine', include_self=False)
-    W = 0.5 * (W + W.T)
-    g = Graph.Weighted_Adjacency(W.todense(), mode='undirected', attr='weight', loops=False)
-
-    return g, W
- ''' 
 # KNN
 
 # Importações
