4014cd5e6b66e2e685b49d172802162669c3ad95
Artur Porto <110861215+Artur-Porto@users.noreply.github.com>
2024-08-28 20:53:13 -0300

    Update CKNN.py

--- src/clustering/CKNN.py
+++ src/clustering/CKNN.py
@@ -1,19 +1,8 @@
 from MST import mst_graph
-
-def mst_graph(X, metric):
-
-    if metric == 'cosine':
-        D = cosine_distances(X, X)
-    else:
-        D = euclidean_distances(X, X)
-    adj_directed = minimum_spanning_tree(D).toarray()
-    adj = adj_directed + adj_directed.T
-    adj[adj > 0] = 1
-    np.fill_diagonal(adj,0)
-
-    return csr_matrix(adj)
-
-
+from sklearn.metrics import euclidean_distances
+import numpy as np
+from scipy.sparse import csr_matrix
+from igraph import Graph
 
 def cknn_graph(X, delta, k):
     assert k < X.shape[0]
