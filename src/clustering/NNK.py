fd9c4b3f0e84deb0c04c15373685043c7868f99a
Artur Porto <110861215+Artur-Porto@users.noreply.github.com>
2024-08-29 20:37:48 -0300

    Update NNK.py

--- src/clustering/NNK.py
+++ src/clustering/NNK.py
@@ -1,4 +1,8 @@
 # Graph Construction from Data using Non Negative Kernel regression (NNK Graphs)
+import numpy as np
+from scipy.linalg import cholesky_solver
+from scipy.sparse import coo_matrix
+import igraph
 
 def create_directed_KNN_mask(D, knn_param=10, D_type='distance'):
     if D_type == 'similarity':
