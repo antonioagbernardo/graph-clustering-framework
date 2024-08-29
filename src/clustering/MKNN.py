bbb269aacc8da831ed38284c6eb19d7a1395dd85
Artur Porto <110861215+Artur-Porto@users.noreply.github.com>
2024-08-28 21:10:35 -0300

    Update MKNN.py

--- src/clustering/MKNN.py
+++ src/clustering/MKNN.py
@@ -1,7 +1,23 @@
-from MST import mst_graph
 from sklearn.neighbors import kneighbors_graph
+from sklearn.metrics.pairwise import euclidean_distances, cosine_distances
 from igraph import Graph
+import numpy as np
+from scipy.sparse.csgraph import minimum_spanning_tree
+from scipy.sparse import csr_matrix
 
+def mst_graph(X, metric):
+
+    if metric == 'cosine':
+        D = cosine_distances(X, X)
+    else:
+        D = euclidean_distances(X, X)
+    adj_directed = minimum_spanning_tree(D).toarray()
+    adj = adj_directed + adj_directed.T
+    adj[adj > 0] = 1
+    np.fill_diagonal(adj,0)
+
+    return csr_matrix(adj)
+  
 def components(W):
   g = Graph.Weighted_Adjacency(W.todense(), mode='undirected', attr='weight', loops=False)
   return len(g.connected_components())
